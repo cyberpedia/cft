95786c86e3735e1696e37591642df612dfccbfab
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 09:43:56 +0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 # api/urls.py
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     # Other API paths will be added here
 ]
