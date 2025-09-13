6d166a06adc38efa14c665973e9adeaa6a569073
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 10:00:59 +0800

    Create asgi.py

---
+++ ctf_platform/asgi.py
@@ -0,0 +1,28 @@
+# ctf_platform/asgi.py
+"""
+ASGI config for ctf_platform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from api import routing # Import your app's routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_platform.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
