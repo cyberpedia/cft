a32e4e964001b10fa1bf251c4e6548f898120f55
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 09:48:14 +0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 # api/urls.py
 from django.urls import path
-from .views import RegisterView, ProfileView, ChallengeListView, ChallengeDetailView
+from .views import RegisterView, ProfileView, ChallengeListView, ChallengeDetailView, SubmitFlagView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('challenges/', ChallengeListView.as_view(), name='challenge_list'),
     path('challenges/<int:pk>/', ChallengeDetailView.as_view(), name='challenge_detail'),
+    path('challenges/<int:pk>/submit/', SubmitFlagView.as_view(), name='submit_flag'),
     # Other API paths will be added here
 ]
