94f4fb6c912e5cc6fc2ed15a6d6fae8c2b51cd56
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 10:17:33 +0800

    Create RegisterView.vue

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,13 @@
+# frontend/src/views/RegisterView.vue
+<template>
+  <div class="flex-grow flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-green-400">Register Page</h1>
+  </div>
+</template>
+
+<script setup>
+// Placeholder for RegisterView
+</script>
+
+<style scoped>
+</style>
