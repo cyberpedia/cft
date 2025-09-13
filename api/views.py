9dbf8441bd951b6302bf69b5123e13b7a9f0c5f7
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 09:41:11 +0800

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,15 @@
 # api/views.py
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    """
+    API endpoint for user registration.
+    Allows creation of new User instances without requiring authentication.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,) # Anyone can register
