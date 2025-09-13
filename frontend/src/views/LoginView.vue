43e6fb0326fff8d23dfefdc7c6d812d2f365894c
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 10:17:11 +0800

    Create LoginView.vue

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+# frontend/src/views/LoginView.vue
+<template>
+  <div class="flex-grow flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-purple-400">Login Page</h1>
+  </div>
+</template>
+
+<script setup>
+// Placeholder for LoginView
+</script>
+
+<style scoped>
+</style>
