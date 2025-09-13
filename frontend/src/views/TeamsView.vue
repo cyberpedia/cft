9c0847e5bc89900868af66414c094d7053d4634e
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 10:19:53 +0800

    Create TeamsView.vue

---
+++ frontend/src/views/TeamsView.vue
@@ -0,0 +1,13 @@
+# frontend/src/views/TeamsView.vue
+<template>
+  <div class="flex-grow flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-indigo-400">Teams Page</h1>
+  </div>
+</template>
+
+<script setup>
+// Placeholder for TeamsView
+</script>
+
+<style scoped>
+</style>
