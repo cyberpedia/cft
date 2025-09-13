6c12bd408644f138e1c4a5295de5e4c88602bc0e
cyberpedia <cyberpedia.ops@gmail.com>
2025-09-13 10:05:51 +0800

    Update admin_urls.py

--- api/admin_urls.py
+++ api/admin_urls.py
@@ -1,9 +1,12 @@
 # api/admin_urls.py
 from rest_framework.routers import DefaultRouter
-from .admin_views import UserManagementViewSet, TeamManagementViewSet
+from .admin_views import UserManagementViewSet, TeamManagementViewSet, TagManagementViewSet, ChallengeManagementViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserManagementViewSet)
 router.register(r'teams', TeamManagementViewSet)
+router.register(r'tags', TagManagementViewSet)
+router.register(r'challenges', ChallengeManagementViewSet)
+
 
 urlpatterns = router.urls
